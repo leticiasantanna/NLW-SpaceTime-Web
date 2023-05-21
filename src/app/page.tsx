f6b67e29703f883509f38fc94eca06347926c3f6
Leticia Santanna <leticia.santanna@escolaconquer.com.br>
2023-05-21 18:12:47 -0300

    adjus prettier configs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
-export default function Home() {
-  return <h1>Hello Word</h1>;
-}
+import React from 'react'
 
+export default function Home() {
+  return (
+    <div className="h-screen bg-zinc-950 text-zinc-50">
+      <h1 className="text-4xl font-bold">Hello Word</h1>
+    </div>
+  )
+}
